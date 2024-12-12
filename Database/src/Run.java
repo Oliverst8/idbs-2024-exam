acfbabfd61e0faa033dc023d22489abae7fa105a
stoftot <gryn@itu.dk>
2024-12-13 00:12:30 +0100

    changed name for normelization

--- Database/src/Run.java
+++ Database/src/Run.java
@@ -5,7 +5,7 @@ import java.util.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class NormalizationTool {
+public class Run {
 
     public static void main(String[] args) throws IOException, UnsupportedFlavorException {
         try {
